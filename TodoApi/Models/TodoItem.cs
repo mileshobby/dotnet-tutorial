8215301d20234d2d4eae95f13d4af7b680f66b3a
mileshobby <mhobby16@gmail.com>
2017-07-15 21:34:27 -0700

    add update and post to todocontroller

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -3,13 +3,10 @@ namespace TodoApi.Models
 {
     public class TodoItem
     {
-        public TodoItem()
-        {
-            //basically setter and getter methods on the id class
-            public long Id { get; set; }
-            public string Name { get; set; }
-            public bool IsComplete { get; set; }
+	    //basically setter and getter methods on the id class
+	    public long Id { get; set; }
+	    public string Name { get; set; }
+	    public bool IsComplete { get; set; }
 
-        }
     }
 }
